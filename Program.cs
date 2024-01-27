228327f0b21107f89c7671570495b89024fc0984
billlims <davidlinxf@gmail.com>
2024-01-27 18:51:27 +1100

    get post pust delete api method

--- Program.cs
+++ Program.cs
@@ -31,6 +31,7 @@ List<Game> games = new(){
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+//?get method
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/games", () => games);
 app.MapGet("/games/{id}", (int id) =>
@@ -43,8 +44,11 @@ app.MapGet("/games/{id}", (int id) =>
   return Results.Ok(game);
 })
 .WithName(GetGameEndPointName);
+
+
 //在ASP.NET Core中，WithName 的主要作用是为一个特定的路由端点（endpoint）指定一个易记的名字。
 //这个名字可以看作是给这个端点起的一个别名。
+//? Post
 app.MapPost("/games", (Game game) =>
 {
   game.Id = games.Max(game => game.Id) + 1;
@@ -52,6 +56,34 @@ app.MapPost("/games", (Game game) =>
   //在 Results.CreatedAtRoute 方法中，第一个参数是端点的名称（这里是 /games/{id} 路径的端点），而第二个参数是一个匿名对象，用于提供端点所需的路由参数。
   //在你的具体例子中，game.Id 的值会被用作 id 路由参数，形成了最终的资源URL，即 /games/4
   return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
+});
+
+//? Update put
+app.MapPut("/games/{id}", (int id, Game updateGame) =>
+{
+  Game? existingGame = games.Find(game => game.Id == id);
+  if (existingGame is null)
+  {
+    return Results.NotFound();
+  }
+  existingGame.Name = updateGame.Name;
+  existingGame.Price = updateGame.Price;
+  existingGame.ImageUri = updateGame.ImageUri;
+  existingGame.Genre = updateGame.Genre;
+  existingGame.ReleaseDate = updateGame.ReleaseDate;
+
+  return Results.NoContent();
+});
+
+//? delte endpoint
+app.MapDelete("/games/{id}", (int id) =>
+{
+  Game? existingGame = games.Find(game => game.Id == id);
+  if (existingGame is not null)
+  {
+    games.Remove(existingGame);
+  }
+  return Results.NoContent();
 });
 
 app.Run();
