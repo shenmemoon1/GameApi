8c75bfb07ffe4cd8973843b28ae5d03028878fc2
billlims <davidlinxf@gmail.com>
2024-01-27 21:08:21 +1100

    refactor: used repository pattern

--- EndPoints/GamesEndPoints.cs
+++ EndPoints/GamesEndPoints.cs
@@ -1,40 +1,15 @@
 using GameStore.Api.Entities;
-
+using GameStore.Api.Repositories;
 namespace GameStore.Api.EndPoints;
 
 public static class GamesEndPoints
 {
     const string GetGameEndPointName = "GetGames";
-    static List<Game> games = new(){
-    new Game(){
-        Id = 1,
-        Name = "League of legend",
-        Genre = "Fighting",
-        Price = 19.99M,
-        ReleaseDate = new DateTime(2000, 1,2),
-        ImageUri = "https://placehold.co/100"
-    },
-    new Game(){
-        Id = 2,
-        Name = "MindCraft",
-        Genre = "Casal",
-        Price = 39.99M,
-        ReleaseDate = new DateTime(2011, 1,2),
-        ImageUri = "https://placehold.co/100"
-    },
-    new Game(){
-        Id = 3,
-        Name = "Espect Legend",
-        Genre = "Fighting",
-        Price = 89.99M,
-        ReleaseDate = new DateTime(2006, 1,2),
-        ImageUri = "https://placehold.co/100"
-    },
-
-    };
-
+    //!使用 this关键词 来延申 endpoints接口
     public static RouteGroupBuilder MapGamesEndpoints(this IEndpointRouteBuilder routes)
     {
+        //defin repository instance
+        InMemGamesRepository repository = new();
         //使用mapgroup 更好的管理routes
         //WithParameterValidation 来之第三方库
         var gamegroup = routes.MapGroup("games").WithParameterValidation();
@@ -43,16 +18,13 @@ public static class GamesEndPoints
         // app.MapGet("/games", () => games);
 
         //?get 使用group的方式
-        gamegroup.MapGet("/", () => games);
+        gamegroup.MapGet("/", () => repository.GetAll());
 
         gamegroup.MapGet("/{id}", (int id) =>
         {
-            Game? game = games.Find(game => game.Id == id);
-            if (game is null)
-            {
-                return Results.NotFound();
-            }
-            return Results.Ok(game);
+            Game? game = repository.Get(id);
+            return game is null ? Results.NotFound() : Results.Ok(game);
+
         })
         .WithName(GetGameEndPointName);
 
@@ -61,18 +33,20 @@ public static class GamesEndPoints
         //这个名字可以看作是给这个端点起的一个别名。
         //? Post
         gamegroup.MapPost("/", (Game game) =>
-        {
-            game.Id = games.Max(game => game.Id) + 1;
-            games.Add(game);
-            //在 Results.CreatedAtRoute 方法中，第一个参数是端点的名称（这里是 /games/{id} 路径的端点），而第二个参数是一个匿名对象，用于提供端点所需的路由参数。
-            //在你的具体例子中，game.Id 的值会被用作 id 路由参数，形成了最终的资源URL，即 /games/4
-            return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
-        });
+            {
+                repository.Create(game);
+                //在 Results.CreatedAtRoute 方法中，第一个参数是端点的名称（这里是 /games/{id} 路径的端点），而第二个参数是一个匿名对象，用于提供端点所需的路由参数。
+                //在你的具体例子中，game.Id 的值会被用作 id 路由参数，形成了最终的资源URL，即 /games/4
+                return Results.CreatedAtRoute(GetGameEndPointName, new
+                {
+                    id = game.Id
+                }, game);
+            });
 
         //? Update put
         gamegroup.MapPut("/{id}", (int id, Game updateGame) =>
         {
-            Game? existingGame = games.Find(game => game.Id == id);
+            Game? existingGame = repository.Get(id);
             if (existingGame is null)
             {
                 return Results.NotFound();
@@ -83,16 +57,18 @@ public static class GamesEndPoints
             existingGame.Genre = updateGame.Genre;
             existingGame.ReleaseDate = updateGame.ReleaseDate;
 
+            repository.Update(updateGame);
+
             return Results.NoContent();
         });
 
         //? delte endpoint
         gamegroup.MapDelete("/{id}", (int id) =>
         {
-            Game? existingGame = games.Find(game => game.Id == id);
+            Game? existingGame = repository.Get(id);
             if (existingGame is not null)
             {
-                games.Remove(existingGame);
+                repository.Delete(id);
             }
             return Results.NoContent();
         });
---
+++ Repositories/InMemGamesRepository.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using GameStore.Api.Entities;
+namespace GameStore.Api.Repositories;
+
+public class InMemGamesRepository
+{
+    /*
+    Repository Pattern（仓储模式）是一种软件设计模式，通常用于数据存取层（Data Access Layer）。
+    该模式旨在将数据访问逻辑从业务逻辑中分离出来，提供了一种方式来组织和管理应用程序的数据访问代码。
+
+    在 Repository Pattern 中，每个实体类（或聚合根）都有一个相应的仓储（Repository）。
+    仓储是负责处理该实体类在数据存储中的增、删、改、查等操作的类。仓储层充当业务逻辑和数据库之间的中间层，
+    隐藏了底层数据访问细节，使得业务逻辑层能够专注于业务规则而不用关心具体的数据存储实现。
+
+    Repository Pattern 的主要优点包括：
+
+    抽象数据访问逻辑： 通过使用 Repository Pattern，你可以将数据访问逻辑抽象出来，
+    使得业务逻辑与具体的数据存储实现解耦。这样，你可以更容易地更换或切换底层的数据存储，而不会影响到业务逻辑。
+
+    单一职责原则： 仓储层的存在使得每个类都有一个明确的职责。业务逻辑负责业务规则，而仓储负责数据访问。
+
+    测试性： 通过使用 Repository Pattern，你可以更容易地进行单元测试，
+    因为你可以使用模拟对象或者内存中的存储来测试业务逻辑，而不需要依赖于真实的数据库
+    */
+    private readonly List<Game> games = new(){
+        new Game(){
+            Id = 1,
+            Name = "League of legend",
+            Genre = "Fighting",
+            Price = 19.99M,
+            ReleaseDate = new DateTime(2000, 1,2),
+            ImageUri = "https://placehold.co/100"
+        },
+        new Game(){
+            Id = 2,
+            Name = "MindCraft",
+            Genre = "Casal",
+            Price = 39.99M,
+            ReleaseDate = new DateTime(2011, 1,2),
+            ImageUri = "https://placehold.co/100"
+        },
+        new Game(){
+            Id = 3,
+            Name = "Espect Legend",
+            Genre = "Fighting",
+            Price = 89.99M,
+            ReleaseDate = new DateTime(2006, 1,2),
+            ImageUri = "https://placehold.co/100"
+        },
+
+    };
+
+    //get all games
+    public IEnumerable<Game> GetAll()
+    {
+        return games;
+    }
+
+
+    //get a game by id
+    public Game? Get(int id)
+    {
+        return games.Find(game => game.Id == id);
+    }
+
+    //create a new game
+    public void Create(Game game)
+    {
+        game.Id = games.Max(game => game.Id) + 1;
+        games.Add(game);
+    }
+
+    //update the game
+    public void Update(Game updateGame)
+    {
+        int index = games.FindIndex(game => game.Id == updateGame.Id);
+        games[index] = updateGame;
+    }
+
+    //delete the game
+    public void Delete(int id)
+    {
+        int index = games.FindIndex(game => game.Id == id);
+        games.RemoveAt(index);
+    }
+}
